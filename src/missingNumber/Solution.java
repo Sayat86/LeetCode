bd1f5e74591b6bc40ef7fb8474d89e6124e364b1
Sayat <saatb458@gmail.com>
2024-06-20 21:51:48 +0600

    add missingNumber

---
+++ src/missingNumber/Solution.java
@@ -0,0 +1,28 @@
+package missingNumber;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {3, 0, 1};
+        System.out.println(solution.missingNumber(nums1)); // 2
+
+        int[] nums2 = {0, 1};
+        System.out.println(solution.missingNumber(nums2)); // 2
+
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println(solution.missingNumber(nums3)); // 8
+    }
+}
